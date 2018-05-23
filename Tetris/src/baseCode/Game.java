5fb726f7059a7bdf2cf74b22b1979de59c60b7de
323911842 <323911842@C-718T0R1.tdsb.on.ca>
2018-05-23 14:40:26 -0400

    Pushing tetris baseCode

---
+++ Tetris/src/baseCode/Game.java
@@ -0,0 +1,53 @@
+package baseCode;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
+import javafx.stage.Stage;
+
+public class Game extends Application{
+	public static void main (String[] args) {
+		launch(args);
+		
+	}
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Group group = new Group();
+		Scene scene = new Scene(group, 450, 600);
+		primaryStage.setTitle("Tetris");
+		Canvas canvas = new Canvas(450, 600);
+		final GraphicsContext gc = canvas.getGraphicsContext2D();
+		
+		Button start = new Button("Start");
+		Button instructions= new Button("Rules");
+		start.setLayoutX(200);
+		start.setLayoutY(270);
+		instructions.setLayoutX(200);
+		instructions.setLayoutY(295);
+		group.getChildren().add(start);
+		group.getChildren().add(instructions);
+		
+		 GridPane gridpane = new GridPane();
+	        for (int i = 0; i < 10; i++) {
+	            RowConstraints row = new RowConstraints(60);
+	            gridpane.getRowConstraints().add(row);
+	        }
+	    
+	        
+	        group.getChildren().add(gridpane);
+
+		canvas.setFocusTraversable(true);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	
+
+}
---
+++ Tetris/src/baseCode/MovingObject.java
@@ -0,0 +1,189 @@
+package baseCode;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+/**
+ 
+ * @authorHansonNguyen
+ */
+public abstract class MovingObject implements Runnable {
+
+	/**
+	 * The x location of the object.
+	 */
+	double x;
+	/**
+	 * The y location of the object.
+	 */
+	double y;
+	/**
+	 * The x speed of the object.
+	 */
+	double xSpeed;
+	/**
+	 * The y speed of the object.
+	 */
+	double ySpeed;
+	/**
+	 * The left edge for bouncing.
+	 */
+	int left;
+	/**
+	 * The right edge for bouncing.
+	 */
+	int right;
+	/**
+	 * The top edge for bouncing.
+	 */
+	int top;
+	/**
+	 * The bottom edge for bouncing.
+	 */
+	int bottom;
+	/**
+	 * Length of pause between position updates. Related to speed of object.
+	 * (Defaults to 10).
+	 */
+	int pauseDuration;
+	/**
+	 * Object color (defaults to black)
+	 */
+	Color color;
+	/**
+	 * Set to false to stop the thread.
+	 */
+	boolean moving;
+
+	/**
+	 * Sets default color and pauseDuration values. Sets speed to 0. Starts
+	 * thread. Every subclass of MovingObject must use this constructor.
+	 * 
+	 * @param x
+	 *            Initial x position.
+	 * @param y
+	 *            Initial y position.
+	 * @param left
+	 *            Left edge for bouncing.
+	 * @param right
+	 *            Right edge for bouncing.
+	 * @param top
+	 *            Top edge for bouncing.
+	 * @param bottom
+	 *            Bottom edge for bouncing.
+	 */
+	public MovingObject(double x, double y, int left, int right, int top,
+			int bottom) {
+		this.pauseDuration = 30;
+		this.xSpeed = 0;
+		this.ySpeed = 0;
+		this.color = Color.BLACK;
+		this.x = x;
+		this.y = y;
+		this.left = left;
+		this.right = right;
+		this.top = top;
+		this.bottom = bottom;
+		startThread();
+	}
+
+	/**
+	 * Starts the movement thread.
+	 */
+	public void startThread() {
+		moving = true;
+		Thread t = new Thread(this);
+		t.start();
+	}
+
+	/**
+	 * Stops the movement thread by terminating the main loop in run().
+	 */
+	public void stopThread() {
+		moving = false;
+	}
+
+	/**
+	 * Updates the x and y values in an infinite loop. If object hits an edge, x
+	 * or y speed is reversed as appropriate.
+	 */
+	public void run() {
+		while (moving) {
+			animateOneStep();
+			x += xSpeed;
+			y += ySpeed;
+			if (x >= right | x <= left)
+				xSpeed *= -1;
+			if (y >= bottom | y <= top)
+				ySpeed *= -1;
+			try {
+				Thread.sleep(pauseDuration);
+			} catch (InterruptedException e) {
+			}
+		}
+	}
+
+	/**
+	 * Draws the object.
+	 * 
+	 * @param g
+	 *            The graphics context
+	 */
+	abstract public void draw(GraphicsContext gc);
+
+	/**
+	 * Performs one step of animation.
+	 */
+	abstract public void animateOneStep();
+	
+	/**
+	 * Sets the x speed.
+	 * 
+	 * @param xSpeed
+	 *            New x speed.
+	 */
+	public void setXSpeed(double xSpeed) {
+		this.xSpeed = xSpeed;
+	}
+
+	/**
+	 * Sets the y speed.
+	 * 
+	 * @param ySpeed
+	 *            New y speed.
+	 */
+	public void setYSpeed(double ySpeed) {
+		this.ySpeed = ySpeed;
+	}
+
+	/**
+	 * Sets the x location.
+	 * 
+	 * @param x
+	 *            New x location.
+	 */
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	/**
+	 * Sets the y location.
+	 * 
+	 * @param y
+	 *            New y location.
+	 */
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	/**
+	 * Sets color of object.
+	 * 
+	 * @param color
+	 *            New color.
+	 */
+	public void setColor(Color color) {
+		this.color = color;
+	}
+}
+
