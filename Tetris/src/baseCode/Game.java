408c4d1b8c2934f3f7f7c44fd145270b6e17cdf7
333716231 <333716231@C-DS23WR1.tdsb.on.ca>
2018-05-24 14:30:03 -0400

    basic unworking hit detect set up

--- Tetris/src/baseCode/Game.java
+++ Tetris/src/baseCode/Game.java
@@ -7,15 +7,25 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
 import javafx.stage.Stage;
 
 public class Game extends Application{
+	
+	
+	
+	
+	final int pauseDuration = 40;
+	static long numBlocks = 0;
+	
 	public static void main (String[] args) {
 		launch(args);
 		
 	}
+	
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
@@ -40,14 +50,43 @@ public class Game extends Application{
 	            RowConstraints row = new RowConstraints(60);
 	            gridpane.getRowConstraints().add(row);
 	        }
-	    
+	        for (int i = 0; i < 10; i++) {
+	            ColumnConstraints col = new ColumnConstraints(60);
+	            gridpane.getColumnConstraints().add(col);
+	        }
 	        
 	        group.getChildren().add(gridpane);
 
+	        Thread game = new Thread(new Runnable() {
+				
+				/**
+				 * Repaints the canvas periodically.
+				 */
+				@Override
+				public void run() {
+					while (true) {
+						for(int i = 0; i > numBlocks; i++) {
+							
+						}
+						draw(gc);
+						try {
+							Thread.sleep(pauseDuration);
+						} catch (InterruptedException e) {
+						}
+					}
+				}
+			});
+	        
+	        
 		canvas.setFocusTraversable(true);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
 	
+	public void draw(GraphicsContext gc) {
+		
+		
+		
+	}
 
 }
